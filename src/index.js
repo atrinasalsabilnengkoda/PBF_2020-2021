39fff6950517ea9a22b42396e97aa0af9cdd8a62
atrinasalsabilnengkoda <atrina.nengkoda@gmail.com>
2021-03-10 10:01:26 +0700

    week-4 practicum1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'
-import HelloComponent from './component/HelloComponent'
-import Test from './Test'
-import Login from './component/Login'
+import App from './App';
+import HelloComponent from './component/HelloComponent';
+import Test from './Test';
+import BlogPost from './container/BlogPost/BlogPost';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './component/Login';
+
 import reportWebVitals from './reportWebVitals';
 
-class StateFullComponent extends React.Component {
-  render() {
-    return <p> StateFullComponents </p>
-  }
-}
+ReactDOM.render(<BlogPost />, document.getElementById('content'));
 
-ReactDOM.render(<Login />, document.getElementById('root'))
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
